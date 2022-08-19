16eb2e399e2d21ceacc3f56dff2788ab7fb47b15
Daniel O'Brien <dob9601@gmail.com>
2022-08-19 16:42:20 +0100

    de-americafy my layout

--- keyboards/sofle/keymaps/dob9601/config.h
+++ keyboards/sofle/keymaps/dob9601/config.h
@@ -4,3 +4,7 @@
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
+#define OLED_TIMEOUT 10000
+#define OLED_FADE_OUT 1 
+#define OLED_FADE_OUT_INTERVAL 1
+
